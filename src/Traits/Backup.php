166023c18eeac761f27a929418f51da18982d19d
Nam Nguyen <phuongnam@phuongnam.org>
2024-03-13 18:58:08 +0700

    update backup

--- src/Traits/Backup.php
+++ src/Traits/Backup.php
@@ -6,16 +6,14 @@ trait Backup
 {
     public function getListBackups($siteId, $page = 1, $limit = 10)
     {
-        $completeUrl    = $this->url . '/data?action=getData&table=backup';
-
-        $data           = $this->encrypt();
+        $data = $this->encrypt();
         $data['type']  = 0;
         $data['p']  = $page;
-        $data['limit']  = $limit;
-        $data['tojs']   = 'get_site_backup';
+        $data['limit'] = $limit;
+        $data['tojs'] = 'get_site_backup';
         $data['search'] = $siteId;
 
-        $result         = $this->httpPostCookie($completeUrl, $data);
+        $result = $this->httpPostCookie($this->url . '/data?action=getData&table=backup', $data);
 
         return json_decode($result, true);
     }
